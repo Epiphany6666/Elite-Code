5e2bcb6f410014794fbb159ee191597279fa1428
只会CV <528375822@qq.com>
2024-11-01 20:49:39 +0800

    后端环境搭建

---
+++ EliteCode-backend/src/main/java/cn/luoyan/elitecode/service/PostThumbService.java
@@ -0,0 +1,30 @@
+package cn.luoyan.elitecode.service;
+
+import cn.luoyan.elitecode.model.entity.PostThumb;
+import com.baomidou.mybatisplus.extension.service.IService;
+import cn.luoyan.elitecode.model.entity.User;
+
+/**
+ * 帖子点赞服务
+ *
+ */
+public interface PostThumbService extends IService<PostThumb> {
+
+    /**
+     * 点赞
+     *
+     * @param postId
+     * @param loginUser
+     * @return
+     */
+    int doPostThumb(long postId, User loginUser);
+
+    /**
+     * 帖子点赞（内部服务）
+     *
+     * @param userId
+     * @param postId
+     * @return
+     */
+    int doPostThumbInner(long userId, long postId);
+}
