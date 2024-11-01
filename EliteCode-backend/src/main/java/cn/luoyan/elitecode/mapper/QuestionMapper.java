10f6344ceaeb402600933278e5675332a50f4fde
洛言 <528375822@qq.com>
2024-11-02 00:29:12 +0800

    4个表的增删改查

---
+++ EliteCode-backend/src/main/java/cn/luoyan/elitecode/mapper/QuestionMapper.java
@@ -0,0 +1,18 @@
+package cn.luoyan.elitecode.mapper;
+
+import cn.luoyan.elitecode.model.entity.Question;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 52837
+* @description 针对表【question(题目)】的数据库操作Mapper
+* @createDate 2024-11-01 20:53:55
+* @Entity cn.luoyan.elitecode.model.entity.Question
+*/
+public interface QuestionMapper extends BaseMapper<Question> {
+
+}
+
+
+
+
---
+++ EliteCode-backend/src/main/java/cn/luoyan/elitecode/model/dto/questionBankQuestion/QuestionBankQuestionQueryRequest.java
@@ -0,0 +1,39 @@
+package cn.luoyan.elitecode.model.dto.questionBankQuestion;
+
+import cn.luoyan.elitecode.common.PageRequest;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 查询题库题目关联请求
+ *
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class QuestionBankQuestionQueryRequest extends PageRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 题库 id
+     */
+    private Long questionBankId;
+
+    /**
+     * 题目 id
+     */
+    private Long questionId;
+
+    /**
+     * 创建用户 id
+     */
+    private Long userId;
+
+    private static final long serialVersionUID = 1L;
+}
