35e9400b18a1291ecfcaa1938b683900102ece2a
洛言 <528375822@qq.com>
2024-12-04 21:53:44 +0800

    通用布局部分代码

--- elitecode-ui/src/app/layout.tsx
+++ elitecode-ui/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
+import BasicLayout from '@/layouts/BasicLayout';
 
 export default function RootLayout({
   children,
@@ -9,7 +10,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <AntdRegistry>{children}</AntdRegistry>
+        <BasicLayout>
+          <AntdRegistry>{children}</AntdRegistry>
+        </BasicLayout>
       </body>
     </html>
   );
--- elitecode-ui/src/app/page.tsx
+++ elitecode-ui/src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from 'antd';
+
 export default function Home() {
-  return <h1> Hello, Next.js! </h1>;
+  return (
+    <>
+      <h1> Hello, Next.js! </h1>
+    </>
+  );
 }
